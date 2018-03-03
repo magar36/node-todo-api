ff49d4c2283ab470ad535df0a79f6a0b4dcbd421
Mohit Agarwal <mohitagarwal@MacShik4ever.local>
2018-03-03 02:52:54 -0700

    updating mongoose.js for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,13 @@ const mongoose =  require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
+let uriObj = {
+localDb: 'mongodb://localhost:27017/ToDoApp',
+mLab: 'mongodb://arjunsingh.agarwal275:Dec1988@ds155278.mlab.com:55278/apptododb'
+};
+
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
+mongoose.connect(process.env.PORT ?  uriObj.mLab : uriObj.localDb);
 
 module.exports = {
   mongoose
