ee4019687e71097bab4f010dca091cf02269bddf
Mohit Agarwal <mohitagarwal@MacShik4ever.local>
2018-03-03 01:49:05 -0700

    deploying the todo app on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose =  require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://arjunsingh.agarwal275:Dec@1988@ds155278.mlab.com:55278/apptododb' || 'mongodb://localhost:27017/ToDoApp');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
 
 module.exports = {
   mongoose
