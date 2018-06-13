390003da9c5738927241bc90120b7503ec8c9c90
Mohit Agarwal <mohitagarwal@MacShik4ever.local>
2018-06-13 00:05:10 -0700

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var samplePwd = 'mohit123';
+
+//bcrypt async callback:
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(samplePwd, salt, (err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+//var hashedPwd = '$2a$10$zjfo02hx/bMX/uACOkJ0o.8NdhyuWn7E8JoIclXw34mXdphgpRt4i'
+var hashedPwd = '$2a$10$CpQ87bgJNwQZV822p8/F2Oa6DElVs3mWc4RPdVWXYuLaLQ/P0V/E.'
+//var hashedPwd = '$2a$10$7tWVoWMlkF/lsnLk06Ktluo.Yrtnaf2AAHhtyUy2SCgHJUJsde.Zm'
+
+bcrypt.compare(samplePwd, hashedPwd, (err, res) => {
+  console.log(res);
+});
 
 // var name = 'mohit';
 // var hash = SHA256(name).toString();
 // console.log(hash);
 
-var data = {
-  id: 1
-};
+// var data = {
+//   id: 1
+// };
 
 // var token = {
 //   data,
@@ -25,8 +43,12 @@ var data = {
 //   console.log('User not allowed');
 // };
 
-var sign = jwt.sign(data,'123');
-console.log(sign);
-
-var verify = jwt.verify(sign, '123');
-console.log(verify);
+//var sign = jwt.sign(data,'123');
+// console.log(typeof sign);
+// console.log(typeof data);
+// console.log(sign);
+// var verify = jwt.verify(sign, '123');
+//console.log(verify);
+//
+//
+// console.log(jwt.verify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YWE2MzQwZDEzZThmMjgxOWE5YTgzMzYiLCJhY2Nlc3MiOiJhdXRoIiwiaWF0IjoxNTIwODQxNzQxfQ.1wpWD4_jGTI-mJk_xGCob96-bPYU6CCMxJp1TNKGHCs','key123'));
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var userSchema = new mongoose.Schema({
   email: {
@@ -60,6 +61,39 @@ userSchema.methods.generateAuthToken = function(){
     return token;
   });
 };
+
+userSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+try{
+  decoded = jwt.verify(token,'key123');
+} catch(e) {
+  return Promise.rejet();
+};
+
+return User.findOne({
+  '_id': decoded._id,
+  'tokens.token': token,
+  'tokens.access': 'auth'
+});
+
+};
+
+userSchema.pre('save', function (next) {
+  var user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('Users', userSchema);
 
